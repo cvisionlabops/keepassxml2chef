4330d6fd096d7dd67ffe5a05912ce86932b55987
skubriev <skubriev@cvisionlab.com>
2014-04-15 11:39:24 +0400

    initial release 0.1

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
 keepassxml2chef
 ===============
 
-Script to create data bags via chef-vault from exported plaintext xml keepassx file.
+Script to create/update data bags via chef-vault from exported keepassx plaintext xml file on chef server/solo data bags
+
+Create an empty keepassx database. Tested on Keepassx 0.4.3 (ubuntu 12.04)
+
+Create a group (data bag) in root. For example `ldap_users_of_our_domain`
+
+Create data bag item as standart keepassx record's with one level of nesting!
+
+Keepass name is data bag item name.
+`srvadm@ldapserver.example.com`
+
+Username field is username in a data bag item.
+`srvadm`
+
+Password field is plaintext password or other sensitive information in a data bag item.
+`strongplaintextpassword`
+
+Url field is chef server user name's separated by commas (Chef clients or users to be vault admins, can be comma list).
+`admin,alibaba,admin2,sysadmin`
+Default `admin`, can be empty.
+
+Comment field is Chef Server SOLR Search Of Nodes.
+`name:name:*.example.local`
+Cannot be empty!
+
+
+Save keepass db. And export as `KeepassX XML file (*.xml)`
+
+It is understood that the knife is already configured to access the chef server
+
+Configure chef vault in knife.rb! 
+
+Clone repo and run script:
+
+`$ cd`
+`$ git clone https://github.com/cvisionlabops/keepassxml2chef`
+`$ cd keepassxml2chef`
+
+Run creating/updating script:
+
+`$ keepassxml2chef path_to_exported_keepass_plaintext_xml_file`
+
---
+++ keepassxml2chef.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+# help on nokogiri
+# http://ruby.bastardsbook.com/chapters/html-parsing/
+
+if ARGV.empty?
+  puts "Please provide exported xml keepassx plaintext passwords file as first argument."
+  exit
+end
+
+unless system("test -f #{ARGV[0]}")
+  puts "File not found! Exiting. Please provide correct xml keepassx db file"
+  exit
+end
+
+require 'nokogiri'
+
+doc = Nokogiri::HTML(open(ARGV[0]))
+
+doc.css('group entry').each do |entry|
+  bag = entry.parent.css('title')[0].text
+  item = entry.css('username').text
+  pass = entry.css('password').text
+  #acc = entry.css('comment').text
+  entry.css('comment').text.empty? acc = "name:admin" : entry.css('comment').text
+  #adm = entry.css('url').text
+  entry.css('url').text.empty? adm = "admin" : entry.css('url').text
+
+  puts ""
+  puts "Found databag item (keepassx group) #{bag}"
+  puts "	found username: #{item}"
+  puts "	found password: #{pass.tr('?','#')}"
+  puts "	found accessor: #{acc}"
+  cmd_create="knife vault create #{bag} #{item} '{\"#{item}\":\"#{pass}\" }' --admins #{adm} -S #{acc} >/dev/null 2>&1"
+  cmd_update="knife vault update #{bag} #{item} '{\"#{item}\":\"#{pass}\" }' --admins #{adm} -S #{acc} >/dev/null 2>&1"
+  if system("knife data bag show #{bag} >/dev/null 2>&1")
+    unless system(cmd_update)
+	puts "	Error update bag: #{bag} #{item}"
+    else 
+    	puts "	Success updated: #{bag} #{item}"
+    end
+  elsif system(cmd_create)
+	puts "	Success created bag: #{bag} #{item}"
+  else
+    puts "	Error create bag: #{bag} #{item}"
+  end
+end
+
+puts ""
+puts "Remove not required bags manually via knife data bag remove or web ui if you need."
+puts "Chef vault only update/add values to server."
+puts ""
+puts "Don't forget to remove plaintext exported keepassx db at file: #{ARGV[0]}"
